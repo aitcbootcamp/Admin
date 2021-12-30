686b8b973035bb3cff4a4c852abd509b288560de
maghalashvili <dato.maghala@gmail.com>
2021-12-30 18:57:26 +0400

    merge

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import React from 'react';
 import axios from 'axios';
@@ -62,38 +61,5 @@ setValue(e.target.value)
     )
     
   }
-=======
-import React from "react";
-import axios from "axios";
-import "./Addcatgory.css";
-
-import { useState, useEffect } from "react";
-const Category = () => {
-  const [category, setCategory] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://206.189.198.66/get_categories").then((res) => {
-      console.log(res.data.userData);
-      const response = res.data.userData;
-      setCategory(response);
-    });
-  }, []);
-
-  console.log(category);
-
-  return (
-    <ul className="category">
-      <div className="input">
-        <input></input> <button>ADD</button>
-      </div>
-      {category.map((category) => (
-        <li className="title" key={category.id}>
-          {category.title}{" "}
-        </li>
-      ))}
-    </ul>
-  );
-};
->>>>>>> a217779de192230edf5dfa0e8781dedc176a7203
 
 export default Category;
